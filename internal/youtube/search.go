ef4c3853950f49fd71a2c6528fe40bae9569d23a
thinhnguyennt <thinhnguyennt@lifung.com>
2020-05-24 11:01:18 +0700

    ok

--- internal/youtube/search.go
+++ internal/youtube/search.go
@@ -9,7 +9,8 @@ import (
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
-	log "github.com/Sirupsen/logrus"
+	//log "github.com/Sirupsen/logrus"
+	log "https://github.com/open-policy-agent/example-api-authz-go/tree/master/vendor/github.com/sirupsen/logrus"
 )
 
 func SetDebug() {
